052d644e395723235dfac1547e48647648e28b61
Md. Masum Billah <md.masum123@gmail.com>
2022-08-09 03:13:58 +0600

    params
    params

--- src/app/rooms/rooms-booking/rooms-booking.component.ts
+++ src/app/rooms/rooms-booking/rooms-booking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-rooms-booking',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms-booking.component.css']
 })
 export class RoomsBookingComponent implements OnInit {
-
-  constructor() { }
+id:number=0;
+// id$=this.router.params.pipe(map((params)=>params['id']));
+id$=this.router.paramMap.pipe(map((params)=>params.get('id')));
+  constructor(private router:ActivatedRoute) { }
 
   ngOnInit(): void {
+    // this.router.params.subscribe((params)=>{this.id=params['id']});
   }
 
 }
